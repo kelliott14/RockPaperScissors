d210e4d19914444d2162c87c9711e6a2646e14c2
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-09-10 17:19:15 +1000

    a tiny bit of firebase logic is in

--- assets/java.js
+++ assets/java.js
@@ -10,12 +10,75 @@ $(document).ready(function() {
         appId: "1:409316849325:web:6d67206e22effae63bb824"
       };
       
-      firebase.initializeApp(config);
-      
-      
-      // * Create a variable to reference the database
-      var database = firebase.database();
+    firebase.initializeApp(config);
+    
+    var database = firebase.database();
 
-      
+    var playGame;
+    var playerOneSelect;
+    var playerTwoSelect;
+    var user;
+    var thisUser;
+    var uid;
+    var playerOne;
+
+    //on initial load of the page
+    function onLoad(){
+        var pickPlayerOne = $("<button>Player One</button>");
+        var pickPlayerTwo = $("<button>Player Two</button>");
+
+        $(pickPlayerOne).addClass("btn btn-primary btn-block");
+        $(pickPlayerTwo).addClass("btn btn-danger btn-block");
+
+        $(pickPlayerOne).attr("id", "playerOnePick");
+        $(pickPlayerTwo).attr("id", "playerTwoPick");
+
+        $(".playerOneDiv").append(pickPlayerOne);
+        $(".playerTwoDiv").append(pickPlayerTwo);
+
+        playerOneSelect = false;
+        playerTwoSelect = false;
+        
+    //TO-DO: clear chatbox
+    }
+    
+    onLoad();
+
+    $("body").on("click", "#playerOnePick", function(){
+    
+      firebase.auth().signInAnonymously().catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+                });
+        user = firebase.auth().currentUser;
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                var isAnonymous = user.isAnonymous;
+                uid = user.uid;
+            }
+        })
+
+        database.ref("/playerOne").set({
+                user: user.uid,
+      })
+        
+        
+        console.log("player 1 picked")
+      database.ref().on("value", function(snapshot){
+          playerOne = snapshot.val().user;
+          
+      })
+    
+
+        if (playerOne = uid){
+            thisUser = true;
+        }else (thisUser = false)
+
+        console.log(thisUser)
+        console.log(playerOne)
+        console.log(uid)
+    
+
+})
 
     });
